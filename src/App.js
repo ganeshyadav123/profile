f5bc7ff30db2d386cd8f2542353c78428a62f1d1
ganeshyadav123 <ganeshmekala88@gmail.com>
2024-01-28 17:25:28 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React wit Ganesh Yadav
         </a>
       </header>
     </div>
